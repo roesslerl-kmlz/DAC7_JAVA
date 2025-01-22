f1f581852768ee1affa442c105daf6daed99267d
michael.sepp <michael.sepp@kmlz.de>
2025-01-22 16:27:22 +0100

    Kommandozeilenparamter

--- run.cmd
+++ run.cmd
@@ -2,4 +2,4 @@
 echo %1
 set JAVA_HOME=C:\Program Files\Java\jdk-21
 set PATH=%JAVA_HOME%\bin;%PATH%
-java -cp webjob.jar;lib\* de.kmlz.App %1 %2 %3
+java -cp webjob.jar;lib\* de.kmlz.App %1 %2 %3 %4 %5 %6 %7 %8 %9
